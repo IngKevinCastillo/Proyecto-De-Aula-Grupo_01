cef33ff3f9d79ce1cb7cfe24b23e17d081892b39
IngKevinCastillo <kevincastllo24@gmail.com>
2024-03-15 19:52:43 -0500

    Proyecto De Aula Grupo 01
    Este proyecto esta hecho para la clase de Programación de computadores II y tiene como funcionalidad principal la gestion de mesas de un restaurante las cuales son realizadas por reservas implementadas por el usuario.

---
+++ Proyecto Mesas de Restaurante/src/Proyecto/Mesa.java
@@ -0,0 +1,64 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Proyecto;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Fabian Hinojosa
+ */
+public class Mesa {
+    private int numeroMesa;
+    private int numeroDeAsientos;
+    private boolean estado;
+    private ArrayList<Reserva> reserva;
+
+    public Mesa() {
+        this.reserva = new ArrayList<>();
+    }
+
+    public Mesa(int numeroMesa, int numeroDeAsientos) {
+        this();
+        this.numeroMesa = numeroMesa;
+        this.numeroDeAsientos = numeroDeAsientos;
+    }
+
+    public int getNumero() {
+        return numeroMesa;
+    }
+
+    public void setNumeroMesa(int numeroMesa) {
+        this.numeroMesa = numeroMesa;
+    }
+
+    public int getNumeroDeAsientos() {
+        return numeroDeAsientos;
+    }
+
+    public void setNumeroDeAsientos(int numeroDeAsientos) {
+        this.numeroDeAsientos = numeroDeAsientos;
+    }
+
+    public boolean getEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    public ArrayList<Reserva> getReserva() {
+        return reserva;
+    }
+
+    public void setReserva(ArrayList<Reserva> reserva) {
+        this.reserva = reserva;
+    }
+
+    public boolean definirEstado() {
+        return estado;
+    }
+}
---
+++ Proyecto Mesas de Restaurante/src/Proyecto/Ppal.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Proyecto;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+/**
+ *
+ * @author Fabian Hinojosa
+ */
+public class Ppal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Mesa mesa1 = new Mesa(1, 4);
+        Mesa mesa2 = new Mesa(2, 6);
+        Mesa mesa3 = new Mesa(3, 2);
+
+        Reserva reserva = new Reserva("Juan", 4, LocalDate.now(), LocalTime.now());
+        
+        reserva.agregarMesa(mesa1);
+        reserva.agregarMesa(mesa2);
+        reserva.agregarMesa(mesa3);
+        
+        reserva.ocuparMesa(mesa1);        
+        reserva.reservarMesa(reserva, mesa2);
+        reserva.reservarMesa(reserva, mesa1);
+    }
+}
